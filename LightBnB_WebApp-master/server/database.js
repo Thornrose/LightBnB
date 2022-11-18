ce90e99a748436242401b29d9656caf64fb27eac
Cheever Esler <ames.esler@gmail.com>
2022-11-17 22:02:39 -0400

    implemented SQL query method for adding new properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -170,7 +170,7 @@ const getAllProperties = function(options, limit = 10) {
   `;
 
   // testing check
-  console.log(queryString, queryParams);
+  // console.log(queryString, queryParams);
 
   return pool.query(queryString, queryParams)
     .then((result) => {
@@ -189,9 +189,15 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+    .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;`, [Number(property.owner_id), property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, Number(property.cost_per_night) * 100, Number(property.parking_spaces), Number(property.number_of_bathrooms), Number(property.number_of_bedrooms), property.country, property.street, property.city, property.province, property.post_code])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.addProperty = addProperty;
